5eefc6af1ee9b82c590c4ca2af3a068df4e94a7e
hajos8 <rabb.andras@gmail.com>
2026-01-11 13:25:46 +0100

    Implemented disk dragging

--- hanoi/src/main/java/main/hanoi/HanoiController.java
+++ hanoi/src/main/java/main/hanoi/HanoiController.java
@@ -7,18 +7,28 @@ import javafx.scene.shape.Rectangle;
 
 //peg locations 100, 295, 490 and width 10
 
-//disk sizes 130, 160, 190
+//disk sizes 130, 160, 190 (65, 80, 95)
 
 public class HanoiController {
+    @FXML public Rectangle disk1, disk2, disk3;
 
-    public Rectangle disk1, disk2, disk3;
+    public static int[][] diskLayout = {
+            {1, 0, 0},
+            {2, 0, 0},
+            {3, 0, 0}
+    };
 
-    public void selected(MouseEvent mouseEvent) {
+    public void dropDisk(MouseEvent mouseEvent) {
         System.out.println(mouseEvent);
     }
 
-    public void dropped(MouseEvent mouseEvent) {
-        System.out.println(mouseEvent);
-    }
+    public void dragDisk(MouseEvent mouseEvent) {
+        //System.out.println(mouseEvent);
 
+        Rectangle rectangle = (Rectangle) mouseEvent.getSource();
+
+        rectangle.setX(rectangle.getX() + mouseEvent.getX() - rectangle.getX() - rectangle.getWidth() / 2);
+        rectangle.setY(rectangle.getY() + mouseEvent.getY() - rectangle.getY()  - rectangle.getHeight() / 2);
+
+    }
 }
